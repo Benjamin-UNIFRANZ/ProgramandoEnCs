1cabcdf7b2dc577a91b4f09393eb1e3f21dcad90
benjaweb <benjaweb453@gmail.com>
2024-11-06 00:23:26 -0400

    first commit4

--- obj/Debug/net8.0/PrimerProyect.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimerProyect.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerProyect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833de101b34559dec395961b566b2cdf2b4a7ac8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerProyect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerProyect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
